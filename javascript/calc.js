569fb5512a99ac950ef9f190569564c244584e41
Nestor Sotelo <nestorfabiansotello@gmail.com>
2020-02-26 17:05:52 -0300

    agregado la funcion calc y untracking las imagenes y eso

---
+++ javascript/calc.js
@@ -0,0 +1,19 @@
+var numField1 = document.getElementById('numField1');
+var numField2 = document.getElementById('numField2');
+var resultField = document.getElementById('resultField');
+var form = document.getElementById('xIsWhatPercentOfY');
+
+form.addEventListener('submit', function(){
+    
+})
+
+
+// function dropdownMenu() {
+//      var x = document.getElementById("dropdownClick");
+//      if (x.className === "topnav") {
+//          x.className += " responsive";
+//          /* cambiar topvan a topnav.responsibe*/
+//      } else {
+//          x.className = "topnav";
+//      }
+//  }
--- javascript/learn-to-code.js
+++
@@ -1,9 +0,0 @@
- function dropdownMenu() {
-     var x = document.getElementById("dropdownClick");
-     if (x.className === "topnav") {
-         x.className += " responsive";
-         /* cambiar topvan a topnav.responsibe*/
-     } else {
-         x.className = "topnav";
-     }
- }
